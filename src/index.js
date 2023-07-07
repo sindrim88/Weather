3689333a734cdeedd0388edd4120d617278abd20
sindrim88 <sindrim88@gmail.com>
2023-07-07 21:22:37 +0000

    .

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 3000;
 
 
 //Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../../public')
-const viewsPath = path.join(__dirname, '../../templates/views');
-const partialsPath = path.join(__dirname, '../../templates/partials');
-const footerPath = path.join(__dirname, '../../templates/footer');
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+const footerPath = path.join(__dirname, '../templates/footer');
 
 
 //Setup handlebars engine and views location
